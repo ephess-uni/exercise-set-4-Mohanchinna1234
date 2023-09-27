84d0ee099fa8fff89c7235985e1beccf4fec975f
Mohanchinna1234 <143284593+Mohanchinna1234@users.noreply.github.com>
2023-09-27 20:31:52 +0530

    Update ex_4_0.py

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -14,16 +14,16 @@ def get_shutdown_events(logfile):
     Your docstring here.  Replace the pass keyword below with your implementation
     """
     with open('logfile','r') as file:
-    lst = []
-    ans = []
-    ans1 = []
-    for i in file:
-        j = i.strip().split()
-        lst.append(j)
-    for i in range(0,len(lst)):
-        if len(lst[i]) > 1:
-            if lst[i][-1] == 'initiated.' and lst[i][-2] == 'Shutdown':
-                ans.append(lst[i])
+        lst = []
+        ans = []
+        ans1 = []
+        for i in file:
+            j = i.strip().split()
+            lst.append(j)
+        for i in range(0,len(lst)):
+            if len(lst[i]) > 1:
+                if lst[i][-1] == 'initiated.' and lst[i][-2] == 'Shutdown':
+                    ans.append(lst[i])
     if len(ans) < 1:
         return []
     else:
